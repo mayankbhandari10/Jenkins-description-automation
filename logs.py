4cadceb6b349ea0553255fedc6f797081c63d4b8
Mayank Bhandari <mayank10oct@yahoo.com>
2023-09-17 16:50:43 +0530

    my 2 commit

--- logs.py
+++ logs.py
@@ -1,4 +1,5 @@
 import sys
+import re  # Import the 're' module for regular expressions
 
 # Read the build log from stdin
 build_log = sys.stdin.read()
@@ -6,8 +7,8 @@ build_log = sys.stdin.read()
 # Define the pattern to search for
 pattern = r"java\.io\.IOException: CreateProcess error=\d+, The system cannot find the file specified"
 
-# Search for the pattern in the build log
-if pattern in build_log:
+# Search for the pattern in the build log using regular expressions
+if re.search(pattern, build_log):
     # Print the pattern if found
     print(f"Pattern Found: {pattern}")
     sys.exit(1)  # Exit with a non-zero status code to indicate failure
