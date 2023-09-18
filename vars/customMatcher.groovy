f134044023996deaf05da7e07ebf41cf74f9b545
Mayank Bhandari <mayank10oct@yahoo.com>
2023-09-18 19:08:33 +0200

    Update customMatcher.groovy

--- vars/customMatcher.groovy
+++ vars/customMatcher.groovy
@@ -3,12 +3,14 @@
 def call(Map config) {
     def logs = config.logs
     def patterns = [
-        'exception': 'Caused: java\\.io\\.IOException: Cannot run program "nohup"',
-        'Error 2': 'script returned exit code 127',
-        'not found': 'groovy\\.lang\\.MissingPropertyException: No such property: \\w+ for class: groovy\\.lang\\.Binding',
-        'maths error': 'java\.lang\.ArithmeticException: Division by zero',
-        'Assertation error': 'java.lang.AssertionError',
-        'executor error': 'jenkins.util.ErrorLoggingExecutorService'
+    'exception': 'Caused: java.io.IOException: Cannot run program "nohup"',
+    'Error 2': 'script returned exit code 127',
+    'not found': 'groovy.lang.MissingPropertyException: No such property: \\w+ for class: groovy.lang.Binding',
+    'maths error': 'java.lang.ArithmeticException: Division by zero',
+    'Assertation error': 'java.lang.AssertionError',
+    'executor error': 'jenkins.util.ErrorLoggingExecutorService'
+]
+
        
         //o Add more patterns as needed
     ]
