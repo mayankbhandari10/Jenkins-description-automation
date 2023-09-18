de3ab5bdda20fb5a8abe822fe8380c4090b5da03
Mayank Bhandari <mayank10oct@yahoo.com>
2023-09-18 18:41:34 +0530

    Added the IAC

---
+++ customMatcher.groovy
@@ -0,0 +1,22 @@
+// vars/customMatcher.groovy
+
+def call(Map config) {
+    def logs = config.logs
+    def patterns = [
+        'exception': 'Caused: java\\.io\\.IOException: Cannot run program "nohup"',
+        'Error 2': 'error_pattern2'
+        // Add more patterns as needed
+    ]
+
+    def matchingLines = []
+
+    logs.each { line ->
+        patterns.each { name, regex ->
+            if (line =~ regex) {
+                matchingLines.add("Matching Pattern: ${name}\n${line}")
+            }
+        }
+    }
+
+    return matchingLines.join('\n')
+}
