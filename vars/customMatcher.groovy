72414fddc451167b6de9e0f31c08624fdd6d085e
Mayank Bhandari <mayank10oct@yahoo.com>
2023-09-18 19:03:00 +0200

    Update customMatcher.groovy

--- vars/customMatcher.groovy
+++ vars/customMatcher.groovy
@@ -6,6 +6,9 @@ def call(Map config) {
         'exception': 'Caused: java\\.io\\.IOException: Cannot run program "nohup"',
         'Error 2': 'script returned exit code 127',
         'not found': 'groovy\\.lang\\.MissingPropertyException: No such property: \\w+ for class: groovy\\.lang\\.Binding',
+        'maths error': 'java\.lang\.ArithmeticException: Division by zero',
+        'Assertation error': 'java.lang.AssertionError',
+        'executor error': 'jenkins.util.ErrorLoggingExecutorService'
        
         //o Add more patterns as needed
     ]
