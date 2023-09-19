a93dae77aff84a5785c7852af52cba845324e360
Mayank Bhandari <mayank10oct@yahoo.com>
2023-09-19 15:44:50 +0200

    Update customMatcher.groovy

--- vars/customMatcher.groovy
+++ vars/customMatcher.groovy
@@ -1,23 +1,25 @@
 def showDescription(Map config) {
-    text='java.lang.ArithmeticException: Division by zero'
-    keyword="ArithmeticException"
+    text = 'java.lang.ArithmeticException: Division by zero'
+    keyword = "ArithmeticException"
     
     def logs = config.logs
     def patterns = [
-        //'exception': 'Caused: java.io.IOException: Cannot run program "nohup"',
-        //'Error 2': 'script returned exit code 127',
-        //'not found': 'groovy.lang.MissingPropertyException: No such property: \\w+ for class: groovy.lang.Binding',
         'maths error': 'java.lang.ArithmeticException: Division by zero',
-        //'Assertation error': 'java.lang.AssertionError',
-        //'executor error': 'jenkins.util.ErrorLoggingExecutorService'
+        'Assertation error': 'java.lang.AssertionError',
+        // Add more patterns here as needed
     ]
 
     def matchingLines = []
-    //match operator
+
     logs.each { line ->
         patterns.each { name, regex ->
             if (line =~ regex) {
-                matchingLines.add("Build Failure: ${name}\n${line}")
+                def message = "Build Failure: ${name}\n${line}"
+                def action = patternActions[name]
+                if (action) {
+                    message += "\n${action(line, keyword)}"
+                }
+                matchingLines.add(message)
             }
         }
     }
@@ -29,7 +31,6 @@ def showDescription(Map config) {
         // If patterns matched, prepend the message to the matching lines
         return "This build failed because of the following reasons:\n${matchingLines.join('\n')}"
     }
-    
 }
 
 def searchAndReturnMessage(line, keyword) {
@@ -39,3 +40,9 @@ def searchAndReturnMessage(line, keyword) {
         return "Keyword '${keyword}' not found in the line."
     }
 }
+
+// Define a map of pattern actions
+def patternActions = [
+    'maths error': { line, keyword -> searchAndReturnMessage(line, keyword) },
+    // Add more pattern actions here as needed
+]
