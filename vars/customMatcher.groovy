6248af40ca2399342ca8c689c1fbb57aacd6b8fa
Mayank Bhandari <mayank10oct@yahoo.com>
2023-09-19 15:18:06 +0200

    Update customMatcher.groovy

--- vars/customMatcher.groovy
+++ vars/customMatcher.groovy
@@ -4,12 +4,12 @@ def showDescription(Map config) {
     
     def logs = config.logs
     def patterns = [
-        'exception': 'Caused: java.io.IOException: Cannot run program "nohup"',
-        'Error 2': 'script returned exit code 127',
-        'not found': 'groovy.lang.MissingPropertyException: No such property: \\w+ for class: groovy.lang.Binding',
+        //'exception': 'Caused: java.io.IOException: Cannot run program "nohup"',
+        //'Error 2': 'script returned exit code 127',
+        //'not found': 'groovy.lang.MissingPropertyException: No such property: \\w+ for class: groovy.lang.Binding',
         'maths error': 'java.lang.ArithmeticException: Division by zero',
-        'Assertation error': 'java.lang.AssertionError',
-        'executor error': 'jenkins.util.ErrorLoggingExecutorService'
+        //'Assertation error': 'java.lang.AssertionError',
+        //'executor error': 'jenkins.util.ErrorLoggingExecutorService'
     ]
 
     def matchingLines = []
