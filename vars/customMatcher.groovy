fc5522955e5ed2f48508a5effd6b7ff2acb38d5a
Mayank Bhandari <mayank10oct@yahoo.com>
2023-09-20 10:05:59 +0200

    Update customMatcher.groovy

--- vars/customMatcher.groovy
+++ vars/customMatcher.groovy
@@ -1,43 +1,41 @@
 def showDescription(Map config) {
-    text='java.lang.ArithmeticException: Division by zero'
-    keyword="ArithmeticException"
-    
     def logs = config.logs
-    def patterns = [
-        //'exception': 'Caused: java.io.IOException: Cannot run program "nohup"',
-        //'Error 2': 'script returned exit code 127',
-        //'not found': 'groovy.lang.MissingPropertyException: No such property: \\w+ for class: groovy.lang.Binding',
-        'Unit test failed': 'java.lang.ArithmeticException: Division by zero',
-        //'Assertation error': 'java.lang.AssertionError',
-        //'executor error': 'jenkins.util.ErrorLoggingExecutorService',
-        'Unit test failed': 'Execution failed for task'
-        
-    ]
+    def buildStatus = config.buildStatus
 
-    def matchingLines = []
-    //match operator
-    logs.each { line ->
-        patterns.each { name, regex ->
-            if (line =~ regex) {
-                matchingLines.add("Build Failure: ${name}\n${line}")
+    // Check if the build status is FAILURE
+    if (buildStatus == 'FAILURE') {
+        def patterns = [
+            //'exception': 'Caused: java.io.IOException: Cannot run program "nohup"',
+            //'Error 2': 'script returned exit code 127',
+            //'not found': 'groovy.lang.MissingPropertyException: No such property: \\w+ for class: groovy.lang.Binding',
+            'Unit test failed': 'java.lang.ArithmeticException: Division by zero',
+            //'Assertation error': 'java.lang.AssertionError',
+            //'executor error': 'jenkins.util.ErrorLoggingExecutorService',
+            'Unit test failed': 'Execution failed for task'
+            // Add more patterns as needed
+        ]
+
+        def matchingLines = []
+        logs.each { line ->
+            patterns.each { name, regex ->
+                if (line =~ regex) {
+                    matchingLines.add("Build Failure: ${name}\n${line}")
+                }
             }
         }
-    }
 
-    if (matchingLines.isEmpty()) {
-        // If no patterns matched, provide a default message
-        return "This build failed because of the following reasons:\nBuild is failed !! Please check the logs for reasons"
+        if (matchingLines.isEmpty()) {
+            return "This build failed because of the following reasons:\nBuild is failed !! Please check the logs for reasons"
+        } else {
+            return "This build failed because of the following reasons:\n${matchingLines.join('\n')}"
+        }
     } else {
-        // If patterns matched, prepend the message to the matching lines
-        return "This build failed because of the following reasons:\n${matchingLines.join('\n')}"
+        // If the build status is not FAILURE, return an empty string
+        return ""
     }
-    
 }
 
-def searchAndReturnMessage(line, keyword) {
-    if (line.contains(keyword)) {
-        return "Build Failure unit test case failed: ${line}"
-    } else {
-        return "Keyword '${keyword}' not found in the line."
-    }
-}
+// Example usage:
+// Call showDescription with the build status and logs
+def description = showDescription(buildStatus: currentBuild.resultIsWorseOrEqualTo('FAILURE') ? 'FAILURE' : 'SUCCESS', logs: currentBuild.rawBuild.getLog(10000).reverse())
+println(description)
